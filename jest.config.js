ebdbf001f542d802a6a46ec456baf718cec12b37
FrederikkeL <fretzo@hotmail.com>
2025-11-18 12:49:48 +0100

    jest update

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 export default {
-  preset: "ts-jest/presets/js-with-ts-esm",
   testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
   globals: {
     "ts-jest": {
       useESM: true,
     },
   },
+  moduleFileExtensions: ["ts", "js", "json", "node"],
   testMatch: ["**/*.test.ts"],
 };
