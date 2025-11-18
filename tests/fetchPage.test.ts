7c3b45eb2b27bf79a6b0b3e9d03435de5ab79254
FrederikkeL <fretzo@hotmail.com>
2025-11-18 12:35:05 +0100

    setup scraper to fetch mock data

---
+++ tests/fetchPage.test.ts
@@ -0,0 +1,25 @@
+import { fetchPage } from "../src/scraperAPI/scraperapi";
+//import axios from "axios";
+
+jest.mock("axios");
+//const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("fetchPage", () => {
+  it("returns mock HTML when useMock is true", async () => {
+    const html: string = await fetchPage(true);
+    expect(html).toContain("<!DOCTYPE html>");
+  });
+
+/*   it("fetches real data when useMock is false", async () => {
+    // Type-safe mock: just tell TypeScript this is an AxiosResponse
+    const mockHtml = "<html><body>Real page</body></html>";
+    mockedAxios.get.mockResolvedValue({ data: mockHtml });
+
+    const html: string = await fetchPage(false);
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      "https://um.dk/rejse-og-ophold/rejse-til-udlandet/rejsevejledninger/cambodja",
+    ); 
+    expect(html).toBe(mockHtml);
+  });*/
+});
