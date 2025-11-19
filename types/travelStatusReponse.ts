a47f5972dd8f7d4fc2531962f2e8eca3ad708452
lausalber <150662642+lausalber@users.noreply.github.com>
2025-11-19 10:27:01 +0100

    ctrl s

--- types/travelStatusReponse.ts
+++ types/travelStatusReponse.ts
@@ -1,8 +1,7 @@
 export interface TravelStatusReponse {
   country: string;
   travelStatus: string;
-  text: { travelAdvice: string,
-      accordionText: string },
+  text: { travelAdvice: string; accordionText: string };
   statusCode: number;
   timestamp: string;
   version: string;
