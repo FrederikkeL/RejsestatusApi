e53d503c915a2ad1844784902ca71944cf363334
lausalber <150662642+lausalber@users.noreply.github.com>
2025-11-26 14:33:35 +0100

    lint updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
-// @ts-check
-
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
-
+ 
 // Define your ESLint configuration using ES Modules syntax
 export default defineConfig({
   extends: [
@@ -13,5 +10,16 @@ export default defineConfig({
     tseslint.configs.strict,
     tseslint.configs.stylistic,
     eslintConfigPrettier,
+  ],
+  overrides: [
+    // Ignore a specific folder (recursively)
+    {
+      files: [".github\workflows", "node_modules"],
+      rules: {
+        // Disable all rules for this folder
+        "no-console": "off",
+        "no-unused-vars": "off",
+      },
+    },
   ],
 });
