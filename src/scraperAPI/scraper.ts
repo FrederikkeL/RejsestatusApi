eae9914711c337317cd36e99fa9d053a7cf4f699
FrederikkeL <fretzo@hotmail.com>
2025-12-02 14:14:00 +0100

    added caching

--- src/scraperAPI/scraper.ts
+++ src/scraperAPI/scraper.ts
@@ -1,5 +1,21 @@
-import { extractTravelStatus } from "./extractTravelStatus";
+const { extractTravelStatus } = require("./extractTravelStatus");
+const { cacheJSON } = require("./caching");
+import type { CountryListResponse } from "../../types/travelStatusReponse";
 
 async function main(useMock) {
-  console.log(await extractTravelStatus("KZ", useMock));
+  var countryListResponse: CountryListResponse = null;
+  if (useMock) {
+    const country = await extractTravelStatus("FI", useMock);
+    const country2 = await extractTravelStatus("FI", useMock);
+    countryListResponse = {
+      httpCode: 200,
+      retrievedTime: new Date().toISOString(),
+      version: "1.0.0",
+      countries: [country, country2],
+    };
+  } else {
+    countryListResponse = await extractTravelStatus("FI", useMock);
+    cacheJSON(countryListResponse);
+  }
 }
+main(true);
